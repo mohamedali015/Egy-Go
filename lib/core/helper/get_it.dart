4840a2e4db18c42aa9c9bd9cab116b77ff4a82a1
Mohamed Ali <mohamed.ali01537@gmail.com>
2025-12-07 17:03:14 +0200

    Add caching mechanism with CacheHelper, CacheData, and ApiResponse classes

--- lib/core/helper/get_it.dart
+++ lib/core/helper/get_it.dart
@@ -1,9 +1,9 @@
+import 'package:egy_go/core/network/api_helper.dart';
 import 'package:get_it/get_it.dart';
 
 
 final getIt = GetIt.instance;
 
 void setupGetIt() {
-  // getIt.registerSingleton<AuthService>(FirebaseAuthService());
-
+  getIt.registerSingleton<ApiHelper>(ApiHelper());
 }
---
+++ lib/core/network/end_points.dart
@@ -0,0 +1,4 @@
+abstract class EndPoints {
+  static const String baseUrl =
+      'https://nti-ecommerce-api-production-9b78.up.railway.app/api/';
+}
